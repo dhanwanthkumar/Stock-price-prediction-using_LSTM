48f9ceea7cb22b9a92c02adf36b892d014ef6586
Muthukumaran <muthukumarancj@gmail.com>
2024-05-17 13:31:35 +0530

    python file is added

---
+++ sequence.py
@@ -0,0 +1,7 @@
+def create_sequences(data, window_size):
+    X = []
+    y = []
+    for i in range(len(data) - window_size):
+        X.append(data[i:i+window_size])
+        y.append(data[i+window_size])
+    return np.array(X), np.array(y)
